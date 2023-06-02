582d37db4b37fba79cf3fd3d19962a0caf691493
GFSprog40 <gustavo.prog40@gmail.com>
2023-06-02 13:24:38 -0300

    indo

---
+++ src/controllers/postController.ts
@@ -0,0 +1,57 @@
+import { PrismaClient } from "@prisma/client"
+import { Request, Response } from "express"
+
+var cloudinary = require('cloudinary')
+
+var imagem = ''
+var resultado = ''
+
+
+const prisma = new PrismaClient()
+
+async function RegisterPost(req: Request, res: Response) {
+  try {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    })
+
+    cloudinary.uploader.upload(req.file?.path, function (result: any, error: any) {
+      imagem = result.secure_url
+      resultado = result
+      console.log(resultado)
+    })
+
+    const data = await prisma.posts.create({
+      data: {
+        title: req.body.title,
+        image: imagem,
+        text: req.body.text,
+        desc: req.body.desc,
+        likes: Number(req.body.likes),
+        views: Number(req.body.views),
+        author: req.body.author
+      }
+    })
+
+    return res.status(201).send({ msg: "Success!", data })
+  } catch (error) {
+    return res.status(400).send({ msg: "Error!", error })
+  }
+}
+async function getAllPosts(req: Request, res: Response) {
+  try {
+    const data = await prisma.posts.findMany()
+
+    return res.status(201).send({ data })
+
+  } catch (error) {
+
+    return res.status(400).send({ msg: "Error!", error })
+
+  }
+}
+
+
+export default { getAllPosts, RegisterPost }
--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,15 @@ import express, { Router, Request, Response } from 'express'
 
 const routes = Router()
 
+import uploadConfig from './config/uploadConfig'
+import multer from 'multer'
+import postController from './controllers/postController'
 
+const upload = multer(uploadConfig)
+
+
+routes.post('/register', upload.single('image'), postController.RegisterPost)
+routes.get('/get-all-posts', postController.getAllPosts)
 
 routes.get("/", (req: Request, res: Response) => {
   return res.status(200).send({ Message: ` App Running on Get` })
