14b55c3dba6a02bda02bbfcfdf5c2e1f9fc1c4aa
GFSprog40 <gustavo.prog40@gmail.com>
2023-06-05 16:15:35 -0300

    deu fora

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,9 @@ const { PORT } = process.env
 const app = express()
 
 
-app.use(cors())
+app.use(cors({
+  origin: "https://junho-blog.netlify.app"
+}))
 app.use(express.json())
 app.use(routes)
 
